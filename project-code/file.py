4fd510d0374b50bc4422b0473b123b3dc951f472
heliakrd <helia8766@gmail.com>
2025-05-23 14:40:14 +0330

    Update file.py
    در کانستراکتور:
    1- name  با  .txt  تموم بشه
    2- بهتره یک ویژگی برای نگهداری مسیر با فولدر والد در نظر گرفته بشه
    
    در متد  editline_command:
    1-  برای اعلام خطا بهتره از rais  استفاده بشه
    2-  line - 1  میتونه مستقیما برای ایندکس استفاده بشه
    
    متد deline_command  هم مشکلات متد بالا رو داشت
    
    در متد  rename:
    چک کنه نام جدید با .txt  تموم بشه
    اگر فایل در فولدر خاصی باشه مطمئن شو اسم جدید با بقیه اسم ها تداخل نداشته باشه
    
    از متد str  هم برای خوانایی بهتر فایل استفاده کردم

--- project-code/file.py
+++ project-code/file.py
@@ -1,7 +1,10 @@
 class File:
-    def __init__(self, name, contents:list[str] = None):
+    def __init__(self, name, contents:list[str] = None, parent = None):
+        if not name.endswith(".txt"):
+            raise ValueError("file name must end with .txt")
         self.name = name
         self.contents = contents if contents is not None else []
+        self.parent = parent
 
     def nwfiletxt_command(self, contents: list) -> None:
         self.contents.clear()
@@ -13,18 +16,16 @@ class File:
             self.contents.append(content)
 
     def editline_command(self, content: str, line: int) -> None:
-        for i in range(len(self.contents)):
-            if i + 1 == line:
-                self.contents[i] = content
-                return
-        print('index out of range')
+        if 1 <= line <= len(self.contents):
+            self.contents[line - 1] = content
+        else:
+            raise IndexError("Line number out of range.")
 
     def deline_command(self, line: int) -> None:
-        for i in range(len(self.contents)):
-            if i + 1 == line:
-                self.contents.pop(i)
-                return
-        print('index out of range')
+        if 1 <= line <= len(self.contents):
+            self.contents.pop(line - 1)
+        else:
+            raise IndexError("Line number out of range.")
 
     def cat_command(self) -> None:
         for content in self.contents:
@@ -32,5 +33,10 @@ class File:
 
 
     def rename_command(self, name: str) -> None:
+        if not name.endswith(".txt"):
+            raise ValueError("File name must end with .txt")
         self.name = name
 
+
+   def __str__(self) -> str:
+       return f"<File {self.name} - {len(self.contents)} lines>"
